f610e560b1b1e9c0054f57d3a71562b134fdc589
leon <ccc@163.com>
2017-04-20 16:13:19 +0800

    init

---
+++ test.js
@@ -0,0 +1,16 @@
+const Locker = require('./index.js');
+const path = require('path');
+const assert = require('assert')
+
+let locker = new Locker(path.resolve(__dirname, '../../cache/changeip.lock'), 1000 * 3);
+locker.lock();
+console.log(locker.isLocked())
+// locker.unlock()
+setTimeout(() => {
+	console.log(locker.isLocked())
+	assert(locker.isLocked() === true)
+}, 2000);
+setTimeout(() => {
+	console.log(locker.isLocked())
+	assert(locker.isLocked() === false)
+}, 4000);
